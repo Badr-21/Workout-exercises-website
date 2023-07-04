3459298d54ddad1ab19a06691d885eeb45cc2a9f
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-04 16:36:30 +0200

    added routes and dynamic routes

--- src/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/AnimatedRoutes/AnimatedRoutes.tsx
@@ -1,21 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import Home from "../pages/Home";
-import Exercises from "../pages/Exercises";
-import About from "../pages/About";
-import { useState } from "react";
-import { Exercise } from "../components/Hero";
-export type Exercises = Exercise[] | [];
+import HomePage from "../pages/HomePage";
+import ExercisesPage from "../pages/ExercisesPage";
+import AboutPage from "../pages/AboutPage";
+import ExerciseDetailsPage from "../pages/ExerciseDetailsPage";
 
 function AnimatedRoutes() {
-   const [exercises, setExercises] = useState<Exercises>([]);
-
    return (
       <AnimatePresence>
          <Routes>
-            <Route path="/" element={<Home exercises={exercises} setExercises={setExercises} />} />
-            <Route path="exercises" element={<Exercises />} />
-            <Route path="about" element={<About />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="exercises-page">
+               <Route index element={<ExercisesPage />} />
+               <Route path="exercise-details-page/:id" element={<ExerciseDetailsPage />} />
+            </Route>
+            <Route path="about-page" element={<AboutPage />} />
          </Routes>
       </AnimatePresence>
    );
