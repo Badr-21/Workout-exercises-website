19ea7fd66990c0caaef970c13b68ac9ea2835a83
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-05 01:45:06 +0200

    added InfinitySpin component from reac-loader-spinner and fixed videoId error

--- src/components/YoutubeVideos.tsx
+++ src/components/YoutubeVideos.tsx
@@ -2,6 +2,8 @@ import { Link, useLocation } from "react-router-dom";
 import { fetchData, youtubeOptions } from "../utils/fetchData";
 import { useEffect, useState } from "react";
 import { Video, VideoData } from "../types";
+import { InfinitySpin } from "react-loader-spinner";
+
 function YoutubeVideos() {
    const [videos, setVideos] = useState<Video[] | []>([]);
    const location = useLocation();
@@ -11,8 +13,11 @@ function YoutubeVideos() {
          `https://youtube-search-and-download.p.rapidapi.com/search?query=${exercise.name}`,
          youtubeOptions
       );
-      const videoDisplayed = videosData.contents.map((videoData: VideoData) => videoData.video);
+      const videoDisplayed = videosData.contents
+         .map((videoData: VideoData) => videoData.video)
+         .filter((video: Video) => video !== undefined);
       setVideos(videoDisplayed);
+      console.log(videoDisplayed);
    };
 
    useEffect(() => {
@@ -24,23 +29,27 @@ function YoutubeVideos() {
          <h2 className="flex font-bold text-3xl text-SafetyOrange mb-8">Youtube videos</h2>
          <div className="flex overflow-x-scroll hide-scroll-bar">
             <div className="flex flex-nowrap py-8">
-               {videos.map((video: Video) => (
-                  <Link
-                     key={video.videoId}
-                     to={`https://www.youtube.com/watch?v=${video.videoId}`}
-                     target="_blank"
-                     className=" hover:cursor-pointer px-4 hover:scale-110 duration-300"
-                  >
-                     <div id={video.videoId} className="inline-block w-[15rem] h-[12rem]">
-                        <img
-                           src={video.thumbnails[0].url}
-                           alt={video.title}
-                           className="w-[15rem] mb-4 rounded-md"
-                        />
-                        <p className="text-SafetyOrange font-bold text-lg px-4">{video.title}</p>
-                     </div>
-                  </Link>
-               ))}
+               {videos.length ? (
+                  videos.map((video: Video) => (
+                     <Link
+                        key={video.videoId}
+                        to={`https://www.youtube.com/watch?v=${video.videoId}`}
+                        target="_blank"
+                        className=" hover:cursor-pointer px-4 hover:scale-110 duration-300"
+                     >
+                        <div id={video.videoId} className="inline-block w-[15rem] h-[12rem]">
+                           <img
+                              src={video.thumbnails[0].url}
+                              alt={video.title}
+                              className="w-[15rem] mb-4 rounded-md"
+                           />
+                           <p className="text-SafetyOrange font-bold text-lg px-4">{video.title}</p>
+                        </div>
+                     </Link>
+                  ))
+               ) : (
+                  <InfinitySpin color="#ff851b" />
+               )}
             </div>
          </div>
       </section>
