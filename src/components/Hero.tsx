3d5af97ccd14691c6ba639b57d281d502dd4127c
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-06 16:30:55 +0200

    made Hero component responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,16 @@ import heroImage from "../assets/hero-image.png";
 
 function Hero() {
    return (
-      <section className="flex justify-between items-center">
+      <section className="flex md:flex-row md:justify-between md:items-center flex-col-reverse items-center md:mb-0 mb-16">
          <div>
-            <h1 className="text-4xl text-SafetyOrange font-bold mb-8">
+            <h1 className="lg:text-4xl text-SafetyOrange lg:font-bold mb-8 font-semibold sm:text-3xl text-2xl">
                Your body achieves what your mind believes.
             </h1>
-            <p className="text-Almond text-3xl font-semibold">
+            <p className="text-Almond lg:text-3xl lg:font-semibold font-medium sm:text-2xl text-xl">
                Check out the most effective exercises personalized to you.
             </p>
          </div>
-         <img className="w-[18rem]" src={heroImage} alt="fitness man" />
+         <img className="lg:w-[18rem] sm:w-[15rem] w-[12rem]" src={heroImage} alt="fitness man" />
       </section>
    );
 }
