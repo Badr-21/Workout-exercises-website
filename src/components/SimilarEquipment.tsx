47be7d6907f4e6baa7c92f83c82433490317d612
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-06 16:38:05 +0200

    made SimilarEquipment responsive

--- src/components/SimilarEquipment.tsx
+++ src/components/SimilarEquipment.tsx
@@ -10,6 +10,7 @@ function SimilarEquipment() {
    const exercise = location.state.displayedExercise;
 
    const FetchEquipmentExercisesData = async () => {
+      setEquipmentExercises([]);
       const similarEquipmentExercises = await fetchData(
          `https://exercisedb.p.rapidapi.com/exercises/equipment/${exercise.equipment}`,
          exercisesOption
@@ -23,7 +24,7 @@ function SimilarEquipment() {
 
    return (
       <section className="flex flex-col m-auto p-auto mb-16">
-         <h2 className="flex font-bold text-3xl text-SafetyOrange mb-8">
+         <h2 className="flex lg:font-bold lg:text-3xl font-semibold md:text-2xl text-xl text-SafetyOrange mb-8">
             Similar equipment exercises
          </h2>
          <div className="flex overflow-x-scroll hide-scroll-bar">
@@ -35,13 +36,13 @@ function SimilarEquipment() {
                         to={`/exercises-page/exercise-details-page/${EquipmentExecise.id}`}
                         state={{ displayedExercise: EquipmentExecise }}
                      >
-                        <div className="rounded-md w-[15rem] hover:cursor-pointer hover:scale-110 duration-300">
+                        <div className="rounded-md md:w-[15rem] w-[12rem] hover:cursor-pointer hover:scale-110 duration-300">
                            <img
                               src={EquipmentExecise.gifUrl}
                               alt={`${EquipmentExecise.name}gif`}
                               className="mb-4 rounded-md"
                            />
-                           <p className="text-SafetyOrange font-bold text-lg px-4 mb-4">
+                           <p className="text-SafetyOrange sm:font-bold md:text-lg text-base font-semibold px-4 mb-4">
                               {EquipmentExecise.name.toUpperCase()}
                            </p>
                         </div>
